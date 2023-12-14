47e34a29b2602d6918080e19769feb861aeddde9
name <email>
2023-12-14 09:26:29 +0100

    add label

--- formularzMafia.py
+++ formularzMafia.py
@@ -10,26 +10,53 @@ class Window(Tk):
 
 class Przycisk(Button):
     def __init__(self, name):
-        super().__init__(text=name, padx=20, pady=20)
-        self.name=name
+        super().__init__(text=name, padx=20, pady=20, command=self.send)
+    def send(self):
+        plik =open('formularz.txt', 'w')
+        plik.write(f'imie: {imie.get()}\nnzawisko: {nazwisko.get()}\nwiek: {wiek.get()}\nDoświadczenie: {doswiadczenie.get()}')
+        plik.close()
 
 class Input(Entry):
      def __init__(self):
         super().__init__()
 
-class Scroll(Scrollbar):
-     def __init__(self):
-        super().__init__()
-        
+class Suwak(Scale):
+     def __init__(self, od, do, orient):
+        super().__init__(to=do, from_=od, orient=orient)
+
+class Licznik(Spinbox):
+     def __init__(self, od, do):
+        super().__init__(to=do, from_=od)
+
+class Napis(Label):
+    def __init__(self, text, rozmiar):
+        super().__init__(text=text, font=('Arial', rozmiar))
+
 okno=Window()
 send=Przycisk('wyślij')
 imie=Input()
-scroll=Scroll()
+nazwisko=Input()
+doswiadczenie=Input()
+scroll=Suwak(1, 20, 'horizontal')
+wiek=Licznik(1, 100)
+Header=Napis('Formularz do mafii', 25)
+Limie=Napis('imie', 10)
+Lnazwisko=Napis('nazwisko', 10)
+Ldoswiadczenie=Napis('Doświadczenie w mafii', 10)
+Lwiek=Napis('wiek', 10)
 
-scroll.set(1, 30)
 
-imie.grid(column=1, row=2)
-send.grid(column=1, row=1)
-scroll.grid(column=1, row=3)
+Header.grid(column=1, row=0, columnspan=3)
+Limie.grid(column=1, row=1)
+Lnazwisko.grid(column=1, row=2)
+Ldoswiadczenie.grid(column=1, row=3)
+Lwiek.grid(column=1, row=4)
+imie.grid(column=2, row=1)
+nazwisko.grid(column=2, row=2)
+doswiadczenie.grid(column=2, row=3)
+scroll.grid(column=1, row=5)
+wiek.grid(column=2, row=4)
+send.grid(column=5, row=3)
 
 okno.mainloop()
+
